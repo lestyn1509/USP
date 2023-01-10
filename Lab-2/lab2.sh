3b26c9ebf47e5b1ecb26e17c5abaa67cd9468dbc
lestyn1509 <110516402+lestyn1509@users.noreply.github.com>
2023-01-11 00:03:43 +0530

    Create lab2.sh

---
+++ Lab-2/lab2.sh
@@ -0,0 +1,19 @@
+ls
+file1.text
+$ cat file1.text 
+hello
+i am Lestyn
+
+$ cat file1.text 
+hello
+i am Pranav
+
+$ cd ..
+$ ls
+dir1		dir2		typescript
+
+$ cd dir1/
+$ ls
+file1.text
+$ cat > file3.text
+this is file 3
